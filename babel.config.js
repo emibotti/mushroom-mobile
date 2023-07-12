28e40af09e76683893c70276dd3fa62969edc0c9
Emiliano Botti <emiliano@xmartlabs.com>
2023-07-12 00:38:10 -0300

    Adds dot-env

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,7 @@ module.exports = {
         root: ['.'],
       },
     ],
+    'module:react-native-dotenv',
     'react-native-reanimated/plugin',
   ],
   presets: ['module:metro-react-native-babel-preset'],
