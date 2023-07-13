f397630d1414a3ad63becb302f75a4c59638b9c3
Emiliano Botti <emiliano@xmartlabs.com>
2023-07-12 22:23:43 -0300

    Removes unnecessary storage/headers reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -3,12 +3,10 @@ import { combineReducers } from '@reduxjs/toolkit'
 import { baseApi } from './APIs'
 import { authApiReducer, authApiReducerPath } from './APIs/auth'
 import { errorReducer } from './storage/error'
-import { headersReducer } from './storage/headers'
 
 export const rootReducer = combineReducers({
   [authApiReducerPath]: authApiReducer,
   error: errorReducer,
-  headers: headersReducer,
   [baseApi.reducerPath]: baseApi.reducer,
 })
 
