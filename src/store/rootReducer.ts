8692cc0193fbf2116f45ea21e665b37a439b6ea2
Emiliano Botti <emiliano@xmartlabs.com>
2023-07-14 01:17:37 -0300

    Removes temporary error reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,11 +2,9 @@ import { combineReducers } from '@reduxjs/toolkit'
 
 import { baseApi } from './APIs'
 import { authApiReducer, authApiReducerPath } from './APIs/auth'
-import { errorReducer } from './storage/error'
 
 export const rootReducer = combineReducers({
   [authApiReducerPath]: authApiReducer,
-  error: errorReducer,
   [baseApi.reducerPath]: baseApi.reducer,
 })
 
