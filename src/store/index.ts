441deb35eebb2ded38490f54a9b0085b5d7e7772
Emiliano Botti <emiliano@xmartlabs.com>
2023-07-13 23:40:22 -0300

    Adds better error handling

--- src/store/index.ts
+++ src/store/index.ts
@@ -13,11 +13,27 @@ import { rootReducer } from './rootReducer'
 
 const middlewares: Middleware[] = [authApi.middleware, baseApi.middleware]
 
+const generateErrorMessage = (payloadData: any): string => {
+  if (typeof payloadData === 'string') {
+    return payloadData as string
+    // TODO: Check it isn't an array
+  } else if (typeof payloadData === 'object') {
+    if ((payloadData as Object).hasOwnProperty('message')) {
+      return payloadData.message
+    } else {
+      return JSON.stringify(payloadData)
+    }
+  } else {
+    return 'Error'
+  }
+}
+
 const rtkQueryErrorLogger: Middleware =
   (_api: MiddlewareAPI) => next => action => {
     if (isRejectedWithValue(action)) {
       if (action.payload) {
-        const errorMessage = action.payload.data
+        // TODO: Maybe in the future we can display localized custom messages depending on the response, instead of showing directly what the backend sends
+        const errorMessage = generateErrorMessage(action.payload.data)
         Alert.alert(errorMessage)
         // TODO: Combining error action with ErrorModal we can display errors all over the app nicely
         // api.dispatch(setErrorAction(errorMessage))
