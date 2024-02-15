8e01e4d7867496eb645e96e026d95fdcd15915d3
Emiliano Botti <emiliano@xmartlabs.com>
2024-02-15 02:11:19 -0300

    Adds cache invaldation when harvesting 2

--- src/store/APIs/mycellium/actions.ts
+++ src/store/APIs/mycellium/actions.ts
@@ -64,6 +64,7 @@ export const harvestMycelium = (builder: Builder) =>
       { id: room_id, type: Tags.Rooms },
       { id: strain_source_id, type: Tags.Events },
       { type: Tags.Statistics },
+      { id: strain_source_id, type: Tags.Mycelium },
     ],
     query: mycelium => ({
       body: mycelium,
